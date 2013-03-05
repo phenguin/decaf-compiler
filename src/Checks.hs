9235d2576eb1cbf9f4a4ea503c594db42acc0725
Phenguin <jcullen@mit.edu>
2013-03-04 21:49:40 -0500

    Add test for array location access validity

--- src/Checks.hs
+++ src/Checks.hs
@@ -40,6 +40,7 @@ checksList = [checkArrayBoundsValid
 	    , checkBreakContinue
 	    , checkMain
 	    , checkAssignOperators
+        , checkArrayLocAccessesValid
 	    ]
 
 testCheck c = doChecks [c] . checkParse
@@ -121,8 +122,8 @@ childsTypePred (MT (_,node ,st) _)  = case node of
               allSame xs = length (nub xs) <= 1
               mcCheck id xs = let res = lookupSymbol id st in
                                case res of
-                                    Nothing -> False
-                                    Just md -> xs == paramTypeList md
+                                    Just md@(MDesc _ _) -> xs == paramTypeList md
+                                    _ -> False
 
 --1 --- Not implementable because of hashmap
 --2 -}
@@ -201,12 +202,16 @@ methodReturnStatement
 						then Nothing
 						else Just $ (show pos) ++ "invalid return"
 			_	-> Down $Just$ foldr (\ a b -> a ++ "\n" ++ b) "" $ Data.Either.lefts returnTypes
-		where 	returnTypes = map returnType (filter isReturn forest)
-			isReturn (MT(_, Return, _) _) = True
-			isReturn _ = False
-			returnType (MT(pos, Return, st) forest) = case forest of 
-				[]	-> Right VoidType
-				_	-> expressionType (head forest)
+		where 	
+        returnTypes = map returnType (filter isReturn $ children block)
+        isReturn (MT(_, Return, _) _) = True
+        isReturn _ = False
+        isBlock (MT(_, DBlock, _) _) = True
+        isBlock _ = False
+        returnType (MT(pos, Return, st) forest) = case forest of 
+            []	-> Right VoidType
+            _	-> expressionType (head forest)
+        block = head $ filter isBlock forest
 
 methodReturnStatement _ = Down Nothing
 checkMethodReturnStatement p =
@@ -215,171 +220,20 @@ checkMethodReturnStatement p =
 -- 10 This is checked coincidentally by check #2.
 
 -- 11 Check id[expr] syntax for validity
-{-
-arrayLocationValidity
-	(MT (pos, (Loc id), st) forest) =
-		case forest of
-			-- Singleton location eg "x"
-			[]	-> Right Nothing
-			-- Array index location eg "x[5]"
-			_	-> if varType != Array
-					then Left "Cannot lookup index of non-array."
-					else
-						case indexType of
-							Left	-> indexType
-							Right	-> if (fromRight indexType) == IntType
-									then Right Nothing
-									else Left "Cannot lookup non-integer index of array."
-			where	varType		= symbolTableType st id
-				indexType 	= expressionType st $ head forest
+arrayLocAccessValid :: SemanticTreeWithSymbols -> TraverseControl
+arrayLocAccessValid (MT (pos, Loc id, _) []) = Down Nothing
+arrayLocAccessValid (MT (pos, Loc id, st) (idx:xs)) = 
+    case isArrayLoc id && (expressionType idx == Right IntType) of
+        True -> Down Nothing
+        False -> Down $ Just $ show pos ++ ": Invalid array index operation."
+    where 
+        isArrayLoc s = case lookupSymbol s st of
+            Just (FDesc (Array _) _) -> True
+            _ -> False
+arrayLocAccessValid _ = Down Nothing
 
-checkArrayLocationValidity p =
-	traverse arrayLocationValidity p
--}{-
--- 12 Expressions within if or while statements must be booleans.
+checkArrayLocAccessesValid = traverse arrayLocAccessValid
 
-ifWhileConditionBoolean
-	(MT (pos, t, st) forest) =
-		case t of
-			If	-> isBoolean
-			While	-> isBoolean
-			_	-> Right Nothing
-		where	isBoolean = case conditionType of
-				Left	-> conditionType
-				Right	-> if (fromRight conditionType) == BoolType
-						then Right Nothing
-						else Left "Cannot use non-boolean condition in control statement."
-			conditionType	= expressionType st $ head forest
-			conditionBlock	= head forest
-
-checkIfWhileConditionBoolean p =
-	traverse ifWhileConditionBoolean p
-
--- 13 The operands of arithmetic operations (+, -, etc.) and relative operations (<, >=, etc.) have to be integers.
--- @TODO: Are we supposed to consider the negation operation here too?
-integerOperation
-	(MT (pos, t, st) forest@(a:b:_)) =
-		case t of
-			Add	-> integerOperands
-			Sub	-> integerOperands
-			Mul	-> integerOperands
-			Div	-> integerOperands
-			Mod	-> integerOperands
-			Lt	-> integerOperands
-			Lte	-> integerOperands
-			Gt	-> integerOperands
-			Gte	-> integerOperands
-			_	-> Right Nothing
-		where 	integerOperands = case aType of
-				Left	-> aType
-				Right	-> case bType of
-					Left	-> bType
-					Right	-> if (fromRight aType) == IntType
-						then if (fromRight bType) == IntType
-							then Right Nothing
-							else Left errorMsg
-						else Left errorMsg
-			aType		= expressionType st a
-			bType		= expressionType st b
-			errorMsg	= "Cannot use non-integer in strictly integer operation."
-
-checkIntegerOperation p =
-	traverse integerOperation p
-
--- 14 The operands of eq-ops must have the same type, either int or boolean.
-
-equalityOperation
-	(MT (pos, t, st) forest@(a:b:_)) =
-		case t of
-			Eql	-> eqOperands
-			Neql	-> eqOperands
-			_	-> Right Nothing
-		where	eqOperands = case aType of
-				Left	-> aType
-				Right	-> case bType of
-					Left	-> bType
-					Right	-> if (fromRight aType) != (fromRight bType)
-							then Left "Cannot compare values of different types."
-							else if (fromRight aType) != IntType && (fromRight aType) != BoolType
-								then Left "Cannot compare values unless they are integers or booleans."
-								else Right Nothing
-			aType 	= expressionType st a
-			bType	= expressionType st b
-
-checkEqualityOperation p =
-	traverse equalityOperation p
-
--- 15 The operands of cond ops and the operand of logical not (!) must have type boolean
-
-booleanOperation
-	(MT (pos, t, st) forest) =
-		case t of
-			And	-> booleanOperands
-			Or	-> booleanOperands
-			Not	-> booleanOperand
-		where	booleanOperands = case aType of
-				Left	-> aType
-				Right	-> case bType of
-					Left	-> bType
-					Right	-> if (fromRight aType) == BoolType && (fromRight bType) == BoolType
-							then Right Nothing
-							else Left errorMsg
-			booleanOperand = case aType of
-				Left	-> aType
-				Right	-> if (fromRight aType) == BoolType
-						then Right Nothing
-						else Left errorMsg
-			aType	= expressionType st a
-			bType	= expressionType st b
-			a	= forest !! 0
-			b	= forest !! 1
-			errorMsg= "Cannot use non-boolean in strictly boolean operation."
-
-booleanOperationCheck p =
-	traverse booleanOperation p
-
--- 16 Assignment ops
--- TODO: I am confused how to properly get the operands ("location" and "expression") for the assignment operation. -Charlie
-
-assignmentType
-	(MT (pos, (Assign id), st) forest) =
-		case exprType of
-			Left -> exprType
-			Right -> if (fromRight exprType) == locationType
-				then Right Nothing
-				else Left "Cannot assign expression to location of a different type."
-		where	locationType	= symbolTableType st location
-			exprType	= expressionType st expr
-			location	= ?????
-			expr		= ????????
-
-assignmentTypeCheck p =
-	traverse assignmentType p
-
--- 17 Increment/decrement assign operation must be integers
--- TODO: I am confused how to properly get the operands ("location" and "expression") for the assignment operation. -Charlie
-
-incrementDecrementAssign
-	(MT (pos, t, st) forest) =
-		case t of
-			AssignPlus -> assignOp
-			AssignMinus -> assignOp
-		where	assignOp 	= case exprType of
-				Left -> exprType
-				Right -> if (fromRight exprType) == IntType && locationType == IntType
-						then Right Nothing
-						else Left "Cannot increment or decrement with non-integers."
-			locationType	= symbolTableType st location
-			exprType	= expressionType st expr
-			location	= ????
-			expression	= ??????
-
-incrementDecrementAssignCheck p =
-	traverse incrementDecrementAssign p
-
--- 18 TODO For statement integer checks
-
--}
 assignCheck (MT (pos, (Assign), st) forest) = 
 	if allequal 
 		then Up Nothing
