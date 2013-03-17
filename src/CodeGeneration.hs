3bbcdfce173256a4930010b8d10ecce2b0f5885b
Santiago Perez <spock@mit.edu>
2013-03-17 19:13:55 -0400

    Added incomplete handlers

--- src/CodeGeneration.hs
+++ src/CodeGeneration.hs
@@ -9,6 +9,7 @@ data MemLoc = Reg Register | BPOffset Int | Label String deriving (Show, Eq)
 data DataSource = P Placeholder | M MemLoc | C Int deriving (Show, Eq) -- Placeholder, memory location, or constant (immediate value)
 data Placeholder = ParamPH | LocalPH deriving (Eq, Show)
 
+
 data AssmOp = Mov DataSource MemLoc
          | CMove Register Register 
          | CMovne Register Register 
@@ -34,3 +35,143 @@ data AssmOp = Mov DataSource MemLoc
          | Ror DataSource MemLoc
          | Cmp DataSource MemLoc
          deriving (Eq, Show)
+
+
+
+
+handler:: STNode -> (SemanticTreeWithSymbols -> [AsmOp])
+handler node = case node of
+            MethodCall _                ->asmMethodCall
+            And                         ->asmAnd
+            Or                          ->asmOr
+            Add                         ->asmAdd
+            Sub                         ->asmSub
+            Mul                         ->asmMul
+            Mod                         ->asmMod
+            Div                         ->asmDiv
+            Not                         ->asmNot
+            Neg                         ->asmNeg
+            AssignPlus                  ->asmAssignPlus
+            AssignMinus                 ->asmAssignMinus
+            Assign                      ->asmAssign
+            Neql                        ->asmNeql
+            Eql                         ->asmEql
+            Lt                          ->asmLt
+            Lte                         ->asmLte
+            Gt                          ->asmGt
+            Gte                         ->asmGte
+            Loc _                       ->asmLoc
+            DStr _                      ->asmDStr
+            DChar _                     ->asmDChar
+            DInt _                      ->asmDInt
+            DBool _                     ->asmDBool
+            DBlock                      ->asmDBlock
+            Return                      ->asmReturn
+            Break                       ->asmBreak
+            Continue                    ->asmContinue
+            If                          ->asmIf
+            For _                       ->asmFor
+            While                       ->asmWhile
+            FD _ _                      ->asmFD
+            CD _                        ->asmCD
+            PD _                        ->asmPD
+            MD _                        ->asmMD
+
+
+
+asmTransform:: SemanticTreeWithSymbols -> AsmOp
+asmTransform node@(MT (pos, stnode, st) _) = (handler stnode) node
+
+asmMethodCall :: SemanticTreeWithSymbols -> [AsmOp]
+asmMethodCall node@(MT (pos, (MethodCall id), st) forest) = 
+
+asmAnd:: SemanticTreeWithSymbols -> [AsmOp]
+asmAnd node@(MT (pos, stnode, st) forest) =
+
+asmOr:: SemanticTreeWithSymbols -> [AsmOp]
+asmOr node@(MT (pos, stnode, st) forest) =
+
+asmAdd:: SemanticTreeWithSymbols -> [AsmOp]
+asmAdd node@(MT (pos, stnode, st) forest) =
+
+asmSub:: SemanticTreeWithSymbols -> [AsmOp]
+asmSub node@(MT (pos, stnode, st) forest) =
+
+asmMul:: SemanticTreeWithSymbols -> [AsmOp]
+asmMul node@(MT (pos, stnode, st) forest) =
+
+asmMod:: SemanticTreeWithSymbols -> [AsmOp]
+asmMod node@(MT (pos, stnode, st) forest) =
+
+asmDiv:: SemanticTreeWithSymbols -> [AsmOp]
+asmDiv node@(MT (pos, stnode, st) forest) =
+
+asmNot:: SemanticTreeWithSymbols -> [AsmOp]
+asmNot node@(MT (pos, stnode, st) forest) =
+
+asmNeg:: SemanticTreeWithSymbols -> [AsmOp]
+asmNeg node@(MT (pos, stnode, st) forest) =
+
+asmAssignPlus:: SemanticTreeWithSymbols -> [AsmOp]
+asmAssignPlus node@(MT (pos, stnode, st) forest) =
+
+asmAssignMinus:: SemanticTreeWithSymbols -> [AsmOp]
+asmAssignMinus node@(MT (pos, stnode, st) forest) =
+
+asmAssign:: SemanticTreeWithSymbols -> [AsmOp]
+asmAssign node@(MT (pos, stnode, st) forest) =
+
+asmNeql:: SemanticTreeWithSymbols -> [AsmOp]
+asmNeql node@(MT (pos, stnode, st) forest) =
+
+asmEql:: SemanticTreeWithSymbols -> [AsmOp]
+asmEql node@(MT (pos, stnode, st) forest) =
+
+asmLt:: SemanticTreeWithSymbols -> [AsmOp]
+asmLt node@(MT (pos, stnode, st) forest) =
+
+asmLte:: SemanticTreeWithSymbols -> [AsmOp]
+asmLte node@(MT (pos, stnode, st) forest) =
+
+asmGt:: SemanticTreeWithSymbols -> [AsmOp]
+asmGt node@(MT (pos, stnode, st) forest) =
+
+asmGte:: SemanticTreeWithSymbols -> [AsmOp]
+asmGte node@(MT (pos, stnode, st) forest) =
+
+asmLoc:: SemanticTreeWithSymbols -> [AsmOp]
+asmLoc node@(MT (pos, stnode, st) forest) =
+
+asmDStr:: SemanticTreeWithSymbols -> [AsmOp]
+asmDStr node@(MT (pos, stnode, st) forest) =
+
+asmDChar:: SemanticTreeWithSymbols -> [AsmOp]
+asmDChar node@(MT (pos, stnode, st) forest) =
+
+asmDInt:: SemanticTreeWithSymbols -> [AsmOp]
+asmDInt node@(MT (pos, stnode, st) forest) =
+
+asmDBool:: SemanticTreeWithSymbols -> [AsmOp]
+asmDBool node@(MT (pos, stnode, st) forest) =
+
+asmDBlock:: SemanticTreeWithSymbols -> [AsmOp]
+asmDBlock node@(MT (pos, stnode, st) forest) =
+
+asmReturn:: SemanticTreeWithSymbols -> [AsmOp]
+asmReturn node@(MT (pos, stnode, st) forest) =
+
+asmBreak:: SemanticTreeWithSymbols -> [AsmOp]
+asmBreak node@(MT (pos, stnode, st) forest) =
+
+asmContinue:: SemanticTreeWithSymbols -> [AsmOp]
+asmContinue node@(MT (pos, stnode, st) forest) =
+
+asmIf:: SemanticTreeWithSymbols -> [AsmOp]
+asmIf node@(MT (pos, stnode, st) forest) =
+
+asmFor:: SemanticTreeWithSymbols -> [AsmOp]
+asmFor node@(MT (pos, stnode, st) forest) =
+
+asmWhile:: SemeanticTreeWithSymbols -> [AsmOp]
+asmWhile node@(MT (pos, stnode, st) forest) =
+
