3797b583a716a69db2b62cc5183c35fc90433d75
Phenguin <jcullen@mit.edu>
2013-03-18 19:03:27 -0400

    Wrong memoryirtree.hs committed.. fixed

--- src/MemoryIRTree.hs
+++ src/MemoryIRTree.hs
@@ -1,17 +1,61 @@
 module MemoryIRTree where
 
 import MultiTree
+import Data.Char (toLower)
 import Semantics
 import Transforms
 import Data.Map as M hiding (map, foldl, filter, singleton)
 
 type LowIRTree = MultiTree IRNode
+type VarBindings = M.Map String MemLoc
+
+data Register = RAX | RBX | RCX | RDX | RSP | RBP | RSI | RDI | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15 deriving (Eq)
+
+instance ValidMemLoc Register where
+    toMemLoc = Reg
+
+class Registerizable a where
+    reg :: Register -> a
+    isReg :: a -> Bool
+    getReg :: a -> Maybe Register
+
+instance Registerizable Register where
+    reg x = x
+    isReg = const True
+    getReg x = Just x 
+
 data MemLoc = Reg Register | BPOffset Int | Label String deriving (Eq)
 
+instance Registerizable MemLoc where
+    reg x = Reg x
+
+    isReg (Reg _) = True
+    isReg _ = False
+
+    getReg (Reg x) = Just x
+    getReg _ = Nothing
 instance Show MemLoc where
 	show (Reg r) = map toLower $ (show r)
 	show (BPOffset i) = (show i)++"(%rbp)"
 	show (Label str) = str
+
+instance Show Register where
+    show RAX = "%rax"
+    show RBX = "%rbx"
+    show RCX = "%rcx"
+    show RDX = "%rdx"
+    show RSP = "%rsp"
+    show RBP = "%rbp"
+    show RSI = "%rsi"
+    show RDI = "%rdi"
+    show R8 = "%r8"
+    show R9 = "%r9"
+    show R10 = "%r10"
+    show R11 = "%r11"
+    show R12 = "%r12"
+    show R13 = "%r13"
+    show R14 = "%r14"
+    show R15 = "%r15"
 
 class ValidMemLoc a where
     toMemLoc :: a -> MemLoc
@@ -58,6 +102,7 @@ data IRNode = ProgL
      deriving (Show, Eq)
 
 convertToLowIRTree :: MultiTree STNode -> LowIRTree
+convertToLowIRTree = undefined
 
 -- Should only be called on a MD node.. will not play well when called at a higher level
 -- bindings for parameters in order
